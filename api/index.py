db1e0e2bcdc5339475aa45377532e3fbe3597ca4
volneifilho <79425059+volneifilho@users.noreply.github.com>
2022-08-28 01:20:31 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,44 +2,23 @@ import os
 
 from flask import (
     Flask,
-    jsonify,
+    render_template,
     request
 )
-from werkzeug.exceptions import BadRequest
+from flask.ext.sqlalchemy import SQLAlchemy
 
 from .utils import add_up
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or \
-    'this-is-super-random-and-crazy'
+db = SQLAlchemy(app)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'GET':
-        return jsonify({"hello": "this is a flask vercel template"})
-    else:
-        try:
-            name = request.json.get("name")
-            status = 200
-        except (AttributeError, BadRequest):
-            name = "name should be in valid JSON"
-            status = 400
-        return jsonify({"hello": name}), status
+    return render_template('index.html')
 
 
-@app.route('/add', methods=['POST'])
-def add():
-    try:
-        x = int(request.json.get("x"))
-        y = int(request.json.get("y"))
-        value = add_up(x, y)
-        status = 200
-    except (AttributeError, BadRequest, TypeError, ValueError):
-        value = "x and y should be numbers in valid JSON"
-        status = 400
-    return jsonify({"result": value}), status
 
 
 if __name__ == "__main__":
